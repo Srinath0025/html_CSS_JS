7ee49ae6859df9e1c997da802bb2666516bcda54
srinath0025 <srinath2571997@gmail.com>
2023-11-08 11:25:55 +0530

    uplode

---
+++ html/class/loop.js
@@ -0,0 +1,18 @@
+for(let i = 0;i<100;i+=1){
+    if(i%5===0)
+    {
+        console.log(i)
+    }
+}
+
+let x =0;
+while(x<10){
+console.log(x)
+    x++;
+}
+
+let y = 50
+do{
+    console.log(y)
+    y++
+}while(y<10)
