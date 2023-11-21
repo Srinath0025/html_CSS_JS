540c621970662f7099e688eeeebfd526c34045a0
srinath0025 <srinath2571997@gmail.com>
2023-11-21 12:16:29 +0530

    uplode

---
+++ html/Dom js/domjs.js
@@ -0,0 +1,22 @@
+const h = document.getElementById("h");
+
+function mybutton () {
+    console.log("first");
+    h1.innerText =  "good morning";
+    h1.style.color= "darkblue"
+}
+function button () {
+    console.log("first");
+    h2.innerText =  "good afternoon";
+    h2.style.color= "pink"
+}
+function myclick () {
+    console.log("first");
+    h3.innerText =  "good evening";
+    h3.style.color= "voilet"
+}
+function btu () {
+    console.log("first");
+    h4.innerText =  "good night";
+    h4.style.color= "gray" 
+}
