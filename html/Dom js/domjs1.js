8c56930dc4145cc7ed687c18d9b91ff33f2a0d82
Srinath0025 <srinath2571997@gmail.com>
2023-11-23 11:14:55 +0530

    uplode

---
+++ html/Dom js/domjs1.js
@@ -0,0 +1,17 @@
+const h = document.getElementById("h");
+
+function mybtu () {
+    console.log("first");
+    h1.innerText = "Name:Srinath.D";
+    h1.style.color= "gray"
+}
+function mybutton () {
+    console.log("first");
+    h2.innerText = "be happy";
+    h2.style.color= "peach"
+}
+function button () {
+    console.log("first");
+    h3.innerText = "you";
+    h3.style.color= "yellow"
+}
